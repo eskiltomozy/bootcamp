8418867e262f6b6b597055b7b36a9fb98db6d5a9
do-wuh <eskil@tomozy.com>
2019-06-11 07:01:04 +0000

    submit added

--- app/controllers/idioms_controller.rb
+++ app/controllers/idioms_controller.rb
@@ -2,4 +2,19 @@ class IdiomsController < ApplicationController
   def index
     @idiom = Idiom.order("RANDOM()").first
   end
+
+  def new
+    @idiom = Idiom.new
+  end
+  
+  def create
+    Idiom.create(idiom_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def idiom_params
+    params.require(:idiom).permit(:expression, :meaning)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'idioms#index'
+  resources :idioms
 end
